bab97e73c0c849c96fceed984268b251a7528ce0
Benradiilyass <Benradi004@gmail.com>
2026-03-30 23:21:42 +0100

    commit 6

--- deploy.ps1
+++ deploy.ps1
@@ -13,4 +13,4 @@ docker rm flask_app -ErrorAction SilentlyContinue
 docker pull $DockerUser/flask_devops_demo:latest
 
 # Lancer le conteneur Flask
-docker run -d --name flask_app -p 5000:5000 $DookerUser/flask_devops_demo:latest
+docker run -d --name flask_app -p 5000:5000 $DockerUser/flask_devops_demo:latest
